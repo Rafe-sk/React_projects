c95c8d9b65ab49cb56f003bf2f1223cd3f819ce8
Rafeshaikh <142775371+Rafe-sk@users.noreply.github.com>
2024-06-21 21:38:20 +0530

    Add files via upload

---
+++ To-Do-List/src/pages/List.jsx
@@ -0,0 +1,12 @@
+import styles from './List.module.css' 
+function List() {
+  return (
+    <>
+        <section className={styles.list}>
+            d
+        </section>
+    </>
+  )
+}
+
+export default List
